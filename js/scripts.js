e92d737a4cf529395d282719a30dfe73bb193af4
Samantha Maturen & Kevin Tokheim <student@epicodus.com>
2015-09-14 16:26:54 -0700

    add extra functionality to app.

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,29 @@ $(document).ready(function(){
 
     var newTask = {taskName: inputtedTask};
 
-    $("div#left-well").append("<li><span class = 'task'>" + newTask.taskName +
-      "<br/><input type='button' class='btn btn-sm btn-danger task-complete' value='Complete Task'></span></li>");
+    $("div#left-well").append("<li class='task-complete'><span class = 'task'>" + newTask.taskName +
+      " <i class='glyphicon glyphicon-ok'></i></span></li>");
 
-    $("input[type=button]").click(function() {
-      $(this).parent().parent().appendTo("#right-well");
-      $(this).remove();
+    $("li.task-complete").click(function() {
+      $(this).toggleClass('task-complete completed');
+      $(this).appendTo("#right-well");
+      $(this).find('.glyphicon').remove();
+      $(this).unbind();
     });
+
+    // $("div#left-well").append("<li id='task-complete'><span class = 'task'>" + newTask.taskName +
+    //   "<button class='btn btn-sm btn-default'><i class='glyphicon glyphicon-ok'></i>Complete</button><br/></span></li>");
+    //
+    //   $("li#task-complete").click(function() {
+    //     $(this).appendTo("#right-well");
+    //
+    //   });
+
+      // <input type='button' class='btn btn-sm btn-danger task-complete' value='Complete Task'>
+    // $("input[type=button]").click(function() {
+    //   $(this).parent().parent().appendTo("#right-well");
+    //   $(this).remove();
+    // });
 
     $("input#new-task-name").val("");
   });
